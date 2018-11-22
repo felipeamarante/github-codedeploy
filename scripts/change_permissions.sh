08644d5224e70571c62c9647575c91e6cb92e0d2
Felipe Amarante <felipeamarante@gmail.com>
2018-11-22 11:32:38 +0200

    initial commit

---
+++ scripts/change_permissions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+
+RUNTIME=$(whoami)
+
+echo "hello from bash"
+echo "This script was ran by ${RUNTIME}"
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+
+RUNTIME=$(whoami)
+
+echo "Hello from Install Dependencies!"
+echo "This script was ran by ${RUNTIME}"
